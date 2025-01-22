08406f7da3df75acdc24b24e4e95356d1a2524b6
Rowren <143919792+Rowren@users.noreply.github.com>
2025-01-22 16:46:01 +0700

    add main

--- lib/pages/bmi.dart
+++ lib/pages/bmi.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:numberpicker/numberpicker.dart';
 
 class Bmi extends StatefulWidget {
   const Bmi({super.key});
@@ -11,7 +12,7 @@ class _BmiState extends State<Bmi> {
   int _height = 120;
 
   Color _color = const Color.fromARGB(255, 99, 215, 230);
-  String _result = '';
+  String _result = 'Result';
 
   @override
   Widget build(BuildContext context) {
@@ -22,9 +23,83 @@ class _BmiState extends State<Bmi> {
       ),
       body: Center(
         child: Column(
-          children: [],
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            //result
+            Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  _result,
+                  style: TextStyle(
+                    color: _color,
+                    fontSize: 22,
+                  ),
+                )),
+            //ส่วนแสดงการทำงานเลือกค่านำ้หนักแล้วส่วนสูง
+            const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Text('โปรดเลือกน้ำหนักแล้วส่วนสูงของท่าน'),
+            ),
+
+            //Row
+            Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                // column weigth
+                Column(
+                  children: [
+                    //title
+                    const Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Text('น้ำหนัก'),
+                    ),
+                    //ตัวเลือกตัวเลข number picker
+                    NumberPicker(
+                      minValue: 20,
+                      maxValue: 150,
+                      value: _weigth,
+                      onChanged: (value) {
+                        setState(() {
+                          _weigth = value;
+                          calculate();
+                        });
+                      },
+                    )
+                  ],
+                ),
+
+                //column hight
+                Column(
+                  children: [
+                    //title
+                    const Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Text('ส่วนสูง'),
+                    ),
+                    //ตัวเลือกตัวเลข number picker
+                    NumberPicker(
+                      minValue: 100,
+                      maxValue: 200,
+                      value: _height,
+                      onChanged: (value) {
+                        setState(() {
+                          _height = value;
+                          calculate();
+                        });
+                      },
+                    )
+                  ],
+                ),
+              ],
+            )
+          ],
         ),
       ),
-      );
+    );
+  }
+
+  //ส่วนของ function คำนวณ BMI
+  calculate() {
+
   }
 }
